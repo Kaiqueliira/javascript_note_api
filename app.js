f40a3cc1bfc5d350f489b2ce3399d6c735ac78f4
Kaique Lira <Kaiqueliira@gmail.com>
2021-12-20 22:18:45 -0300

    instalação e configuração do mongoose

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
+require('./config/database')
 
 var usersRouter = require('./app/routes/users');
 
@@ -9,7 +10,6 @@ var app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/users', usersRouter);
